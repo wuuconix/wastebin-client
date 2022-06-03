b684692befc04bb439667fcea92b002a45bef147
wuuconix <1521900139@qq.com>
2022-06-03 23:51:05 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wastebin-client
-wastebin 配套npm客户端
+
+wastebin 配套命令客户端
+
+使用实例
+
+```bash
+echo "wuuconix" | waste #link: https://paste.wuuconix.link/53d977063c0c488e8c7e1fdfbadefe7c
+cat index.js | waste #link: https://paste.wuuconix.link/fd7f26287592477a84b2fb82f7d88e78
+```
+
---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const { readFileSync } = require("fs")
+const data = readFileSync(0, "utf-8") //得到stdin中传入的字符
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args))
+
+fetch("https://paste.wuuconix.link/documents", {
+    method: "POST",
+    body: new URLSearchParams({data})
+}).then(res => res.json()).then(res => {
+    const filename = res.filename
+    console.log(`link: https://paste.wuuconix.link/${filename}`)
+})
